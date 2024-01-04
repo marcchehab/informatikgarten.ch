49cc38f191497bc31cff69cd1aa7a36e7eb3d9ff
luzmediach <marc@luzmedia.ch>
2024-01-04 11:57:04 +0100

    logger fix

--- components/logger.ts
+++ components/logger.ts
@@ -2,7 +2,7 @@
 enum LogLevels {
     DEBUG,
     INFO,
-    WARN,
+    WARNING,
     ERROR
 }
 const LOG_LEVEL = LogLevels[process.env.LOG_LEVEL] || LogLevels.DEBUG;
@@ -10,9 +10,10 @@ const LOG_LEVEL = LogLevels[process.env.LOG_LEVEL] || LogLevels.DEBUG;
 type LogLevelString = 'DEBUG' | 'INFO' | 'WARNING' | 'ERROR';
 
 export default function log(level: LogLevelString, ...messages) {
-    if (LOG_LEVEL > LogLevels[level]) {
+    if (LogLevels[level] < LOG_LEVEL) {
+        console.log(`${level}:`, "ignored");
         return;
-    } else if (LOG_LEVEL === LogLevels[level]) {
+    } else if (LogLevels[level] == LogLevels.ERROR) {
         console.error(`${level}:`, ...messages);
         return;
     } else {
